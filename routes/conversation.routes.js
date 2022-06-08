1d5973e576385c5784049b0efc1059dcb3bd3862
Gabrielius <gdobrovolskis97@gmail.com>
2022-06-08 20:45:34 +0100

    conversation and its participants

--- routes/conversation.routes.js
+++ routes/conversation.routes.js
@@ -27,31 +27,64 @@ router.post("/create", ensureAuth, async (req, res) => {
 
 router.get("/", ensureAuth, async (req, res) => {
   const userId = req.user.id;
-  const convos = await prisma.user.findFirst({
-    where: { id: userId },
-    include: { conversations: true, user: true },
+  console.log(userId);
+  const convos = await prisma.inbox_Participants.findMany({
+    where: {
+      userId,
+    },
+    include: {
+      inbox_uid: {
+        include: {
+          Inbox_Participants: {
+            where: { NOT: { userId } },
+            include: { user_id: { select: { username: true, id: true } } },
+          },
+        },
+      },
+    },
+  });
+  console.log(convos, convos[3].inbox_uid.Inbox_Participants);
+
+  let inboxArray = [];
+
+  convos.forEach((element) => {
+    let inbox = {};
+    inbox.last_message = element.inbox_uid.last_message;
+    inbox.last_user_id = element.inbox_uid.userId;
+    inbox.participants = [];
+    element.inbox_uid.Inbox_Participants.forEach((participant) => {
+      inbox.participants.push(participant.user_id);
+    });
+    inboxArray.push(inbox);
   });
 
-  res.status(200).json(convos);
+  res.json(inboxArray);
 });
 
 router.get("/inbox", async (req, res) => {
+  const { user2 } = req.body;
+  const user1 = req.user.id;
+  console.log(req.user);
+  console.log(req.body);
+  if ((!user1, !user2)) return res.status(400).send("user missing");
+
+  const userprofile1 = await prisma.user.findUnique({ where: { id: user1 } });
+  const userprofile2 = await prisma.user.findUnique({ where: { id: user2 } });
+  if ((!userprofile1, !userprofile2))
+    return res.status(400).send("user doesnt exist");
+
   const inbox = await prisma.inbox.create({
     data: {
-      id: 5,
-      last_sent_user_id: {
-        connect: { id: "578dc75b-c491-4b13-94ef-f25d96b48480" },
-      },
       Inbox_Participants: {
         create: [
           {
             user_id: {
-              connect: { id: "578dc75b-c491-4b13-94ef-f25d96b48480" },
+              connect: { id: user1 },
             },
           },
           {
             user_id: {
-              connect: { id: "578dc75b-c491-4b13-94ef-f25d96b48480" },
+              connect: { id: user2 },
             },
           },
         ],
