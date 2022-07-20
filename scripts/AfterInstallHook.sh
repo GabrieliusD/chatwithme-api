8279c8770bf02f21f9123cf6bb60a6324d05a0a3
Gabrielius <gdobrovolskis97@gmail.com>
2022-07-20 22:36:36 +0100

    test

--- scripts/AfterInstallHook.sh
+++ scripts/AfterInstallHook.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 cd /home/ec2-user/project
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_copletion (node is in)
+
 npm install
-pm2 start index.js
+pm2 start index.js --name "ChatApi"
