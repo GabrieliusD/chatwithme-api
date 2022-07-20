c72e8c7acac6470f60d570167334f4ba36ccecdb
Gabrielius <gdobrovolskis97@gmail.com>
2022-07-20 22:49:46 +0100

    just work

--- scripts/BeforeInstallHook.sh
+++ scripts/BeforeInstallHook.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-source ~/.nvm/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install 16
